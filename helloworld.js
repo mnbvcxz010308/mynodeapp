67f29e1308bd9696b85dc883420eecee13d52f6c
vipin <vipings007@gmail.com>
2017-05-06 15:51:49 +0530

    modified:   helloworld.js 	modified:   package.json

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,7 @@ const http = require('http');
 const host = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req,res)
-	=> {
+const server = http.createServer((req,res)=> {
 	res.setHeader('Content-Type','text/plain');
 	res.statusCode = 200;
 	res.end('Hello World, from Node JS !');
