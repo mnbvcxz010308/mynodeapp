e685b930ec31d53bfeab51dcda9a1f51ee0f1089
vipin <vipings007@gmail.com>
2017-05-06 15:23:23 +0530

    new file:   helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const host = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res)
+	=> {
+	res.setHeader('Content-Type','text/plain');
+	res.statusCode = 200;
+	res.end('Hello World, from Node JS !');
+
+
+});
+
+server.listen(port,host,()=>{
+console.log(`Server running at https://${host}:${port}/`);
+});
+	
+
